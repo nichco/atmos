e873480d724ad37530838f41935d3e3bfd3e8c2a
Nicholas Orndorff <norndorff@live.com>
2022-09-23 20:09:14 -0700

    seems to work

--- surrogate_explicit.py
+++ surrogate_explicit.py
@@ -1,5 +1,7 @@
 import numpy as np
 import csdl
+import python_csdl_backend
+from surrogate import sm_p, sm_d
 
 
 class Atm(csdl.Model):
@@ -28,3 +30,34 @@ class AtmExplicit(csdl.CustomExplicitOperation):
         # output: pressure and density
         self.add_output('pressure', shape=(1,))
         self.add_output('density', shape=(1,))
+
+        self.declare_derivatives('pressure', 'altitude')
+        self.declare_derivatives('density', 'altitude')
+
+    def compute(self, inputs, outputs):
+
+        # surrogate model
+        pressure = sm_p.predict_values(inputs['altitude'])
+        density = sm_d.predict_values(inputs['altitude'])
+
+        outputs['pressure'] = 1*pressure
+        outputs['density'] = 1*density
+
+    def compute_derivatives(self, inputs, derivatives):
+
+        dp_da = sm_p.predict_derivatives(inputs['altitude'], 0)
+        dd_da = sm_d.predict_derivatives(inputs['altitude'], 0)
+
+        derivatives['pressure', 'altitude'] = 1*dp_da
+        derivatives['density', 'altitude'] = 1*dd_da
+
+
+# run model
+sim = python_csdl_backend.Simulator(Atm(alt=100))
+sim.run()
+
+print(sim['pressure'])
+print(sim['density'])
+
+# print partials
+sim.check_partials(compact_print=True)
